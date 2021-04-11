4f26d9fba99535550851cefb7b622c1d66effb57
IvanFarkas <Ivan.Farkas@3DHDSoft.com>
2021-04-11 13:16:30 -0400

    Updated

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,49 +1,43 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { STLLoader } from 'three/examples/jsm/loaders/STLLoader'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import Stats from 'three/examples/jsm/libs/stats.module'
 
 const scene: THREE.Scene = new THREE.Scene()
-
 const axesHelper = new THREE.AxesHelper(5)
 scene.add(axesHelper)
 
-var light = new THREE.DirectionalLight();
-light.position.set(2.5, 7.5, 15)
-scene.add(light);
-
 const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-camera.position.z = 3
+camera.position.z = 2
 
 const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer()
+renderer.physicallyCorrectLights = true
+renderer.shadowMap.enabled = true
 renderer.outputEncoding = THREE.sRGBEncoding
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
 const controls = new OrbitControls(camera, renderer.domElement)
-controls.enableDamping = true
 
-const envTexture = new THREE.CubeTextureLoader().load(["img/px_25.jpg", "img/nx_25.jpg", "img/py_25.jpg", "img/ny_25.jpg", "img/pz_25.jpg", "img/nz_25.jpg"])
-envTexture.mapping = THREE.CubeReflectionMapping
-
-const material = new THREE.MeshPhysicalMaterial({
-  color: 0xb2ffc8,
-  envMap: envTexture,
-  metalness: .25,
-  roughness: 0.1,
-  transparent: true,
-  transmission: 1.0,
-  side: THREE.DoubleSide,
-  clearcoat: 1.0,
-  clearcoatRoughness: .25
-});
-
-const loader = new STLLoader()
+const loader = new GLTFLoader()
 loader.load(
-  'models/example.stl',
-  function (geometry) {
-    const mesh = new THREE.Mesh(geometry, material)
-    scene.add(mesh)
+  'models/monkey_textured.glb',
+  function (gltf) {
+    gltf.scene.traverse(function (child) {
+      if ((<THREE.Mesh>child).isMesh) {
+        let m = <THREE.Mesh>child
+        m.receiveShadow = true
+        m.castShadow = true
+      }
+      if ((<THREE.Light>child).isLight) {
+        let l = <THREE.Light>child
+        l.castShadow = true
+        l.shadow.bias = -.003
+        l.shadow.mapSize.width = 2048
+        l.shadow.mapSize.height = 2048
+      }
+    })
+    scene.add(gltf.scene);
   },
   (xhr) => {
     console.log((xhr.loaded / xhr.total * 100) + '% loaded')
