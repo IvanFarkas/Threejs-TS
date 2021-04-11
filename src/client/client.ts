38727c124a6de8494711a9884217c762987ab503
IvanFarkas <Ivan.Farkas@3DHDSoft.com>
2021-04-11 18:10:41 -0400

    Updated

--- src/client/client.ts
+++ src/client/client.ts
@@ -2,17 +2,15 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
 import Stats from 'three/examples/jsm/libs/stats.module'
+import { GUI } from 'three/examples/jsm/libs/dat.gui.module'
 
 const scene: THREE.Scene = new THREE.Scene()
 const axesHelper = new THREE.AxesHelper(5)
 scene.add(axesHelper)
 
-let light = new THREE.PointLight();
-light.position.set(0.8, 1.4, 1.0)
+var light = new THREE.PointLight();
+light.position.set(2.5, 7.5, 15)
 scene.add(light);
-
-let ambientLight = new THREE.AmbientLight();
-scene.add(ambientLight);
 
 const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 camera.position.set(0.8, 1.4, 1.0)
@@ -25,28 +23,76 @@ const controls = new OrbitControls(camera, renderer.domElement)
 controls.screenSpacePanning = true
 controls.target.set(0, 1, 0)
 
-const material: THREE.MeshNormalMaterial = new THREE.MeshNormalMaterial()
-
+// AnimationMixer - https://threejs.org/docs/#api/en/animation/AnimationMixer
+let mixer: THREE.AnimationMixer
+let modelReady = false;
+let animationActions: THREE.AnimationAction[] = new Array()
+let activeAction: THREE.AnimationAction
+let lastAction: THREE.AnimationAction
 const fbxLoader: FBXLoader = new FBXLoader();
-fbxLoader.load('models/kachujin_g_rosales.fbx', (object) => {
-  object.traverse(function (child) {
-    if ((<THREE.Mesh>child).isMesh) {
-      // (<THREE.Mesh>child).material = material
-      if ((<THREE.Mesh>child).material) {
-        ((<THREE.Mesh>child).material as THREE.MeshBasicMaterial).transparent = false
-      }
-    }
-  })
+
+// Character - Vanguard By T. Choonyung - https://www.mixamo.com/#/?query=vanguard&type=Character
+fbxLoader.load('models/vanguard_t_choonyung.fbx', (object) => {
   object.scale.set(.01, .01, .01)
+  mixer = new THREE.AnimationMixer(object);
+
+  let animationAction = mixer.clipAction((object as any).animations[0]);
+  animationActions.push(animationAction)
+  animationsFolder.add(animations, "default")
+  activeAction = animationActions[0]
+
   scene.add(object);
-},
-  (xhr) => {
+
+  // Animation- Samba - https://www.mixamo.com/#/?query=Samba&type=Motion%2CMotionPack
+  fbxLoader.load('models/vanguard@samba.fbx', (object) => {
+    console.log("loaded samba")
+
+    let animationAction = mixer.clipAction((object as any).animations[0]);
+    animationActions.push(animationAction)
+    animationsFolder.add(animations, "samba")
+
+    // Animation - Belly Dancing - https://www.mixamo.com/#/?query=Belly+Dancing&type=Motion%2CMotionPack
+    fbxLoader.load('models/vanguard@bellydance.fbx', (object) => {
+      console.log("loaded bellydance")
+      let animationAction = mixer.clipAction((object as any).animations[0]);
+      animationActions.push(animationAction)
+      animationsFolder.add(animations, "bellydance")
+
+      // Animation - Goofy Running - https://www.mixamo.com/#/?query=Goofy+Running&type=Motion%2CMotionPack
+      fbxLoader.load('models/vanguard@goofyrunning.fbx', (object) => {
+        console.log("loaded goofyrunning");
+        (object as any).animations[0].tracks.shift()
+        //console.dir((object as any).animations[0])
+        let animationAction = mixer.clipAction((object as any).animations[0]);
+        animationActions.push(animationAction)
+        animationsFolder.add(animations, "goofyrunning")
+        //console.dir(animationAction)
+
+        modelReady = true
+      }, (xhr) => {
+        console.log((xhr.loaded / xhr.total * 100) + '% loaded')
+      },
+        (error) => {
+          console.log(error);
+        }
+      )
+    }, (xhr) => {
+      console.log((xhr.loaded / xhr.total * 100) + '% loaded')
+    }, (error) => {
+      console.log(error);
+    })
+  }, (xhr) => {
     console.log((xhr.loaded / xhr.total * 100) + '% loaded')
   },
+    (error) => {
+      console.log(error);
+    })
+}, (xhr) => {
+  console.log((xhr.loaded / xhr.total * 100) + '% loaded')
+},
   (error) => {
     console.log(error);
-  }
-)
+  })
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
@@ -59,11 +105,47 @@ function onWindowResize() {
 const stats = Stats()
 document.body.appendChild(stats.dom)
 
+var animations = {
+  default: function () {
+    setAction(animationActions[0])
+  },
+  samba: function () {
+    setAction(animationActions[1])
+  },
+  bellydance: function () {
+    setAction(animationActions[2])
+  },
+  goofyrunning: function () {
+    setAction(animationActions[3])
+  },
+}
+
+const setAction = (toAction: THREE.AnimationAction) => {
+  if (toAction != activeAction) {
+    lastAction = activeAction
+    activeAction = toAction
+    //lastAction.stop()
+    lastAction.fadeOut(1)
+    activeAction.reset()
+    activeAction.fadeIn(1)
+    activeAction.play()
+  }
+}
+
+const gui = new GUI()
+const animationsFolder = gui.addFolder("Animations")
+animationsFolder.open()
+
+const clock: THREE.Clock = new THREE.Clock()
+
 var animate = function () {
   requestAnimationFrame(animate)
 
   controls.update()
 
+  if (modelReady) {
+    mixer.update(clock.getDelta());
+  }
   render()
 
   stats.update()
