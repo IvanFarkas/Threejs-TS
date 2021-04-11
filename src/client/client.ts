00e52482e2a78fe455fa9293b5aa1c387e3ea8a7
IvanFarkas <Ivan.Farkas@3DHDSoft.com>
2021-04-11 13:07:51 -0400

    Updated

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,53 +1,49 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader'
+import { PLYLoader } from 'three/examples/jsm/loaders/PLYLoader'
 import Stats from 'three/examples/jsm/libs/stats.module'
 
 const scene: THREE.Scene = new THREE.Scene()
 const axesHelper = new THREE.AxesHelper(5)
 scene.add(axesHelper)
 
+var light = new THREE.SpotLight();
+light.position.set(20, 20, 20)
+scene.add(light);
+
 const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-camera.position.z = 2
+camera.position.z = 40
 
 const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer()
-renderer.physicallyCorrectLights = true
-renderer.shadowMap.enabled = true
+renderer.outputEncoding = THREE.sRGBEncoding
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
 const controls = new OrbitControls(camera, renderer.domElement)
 
-var dracoLoader = new DRACOLoader();
+const envTexture = new THREE.CubeTextureLoader().load(["img/px_25.jpg", "img/nx_25.jpg", "img/py_25.jpg", "img/ny_25.jpg", "img/pz_25.jpg", "img/nz_25.jpg"]);
+envTexture.mapping = THREE.CubeReflectionMapping;
+const material = new THREE.MeshPhysicalMaterial({
+  color: 0xb2ffc8,
+  envMap: envTexture,
+  metalness: .25,
+  roughness: 0.1,
+  transparent: true,
+  transmission: 1.0,
+  side: THREE.DoubleSide,
+  clearcoat: 1.0,
+  clearcoatRoughness: .25
+});
 
-// TODO: Fix HACK
-dracoLoader.setDecoderPath('https://unpkg.com/three@0.127.0/examples/js/libs/draco/');
-// dracoLoader.setDecoderPath('/js/libs/draco/');
-
-dracoLoader.setDecoderConfig({ type: 'js' });
-
-const loader = new GLTFLoader()
-loader.setDRACOLoader(dracoLoader)
+let mesh: THREE.Mesh
+const loader = new PLYLoader()
 loader.load(
-  'models/monkey_compressed.glb',
-  function (gltf) {
-    gltf.scene.traverse(function (child) {
-      if ((<THREE.Mesh>child).isMesh) {
-        let m = <THREE.Mesh>child
-        m.receiveShadow = true
-        m.castShadow = true
-      }
-      if ((<THREE.Light>child).isLight) {
-        let l = <THREE.Light>child
-        l.castShadow = true
-        l.shadow.bias = -.003
-        l.shadow.mapSize.width = 2048
-        l.shadow.mapSize.height = 2048
-      }
-    })
-    scene.add(gltf.scene)
-
+  'models/sean4.ply',
+  function (geometry) {
+    geometry.computeVertexNormals();
+    mesh = new THREE.Mesh(geometry, material)
+    mesh.rotateX(-Math.PI / 2)
+    scene.add(mesh)
   },
   (xhr) => {
     console.log((xhr.loaded / xhr.total * 100) + '% loaded')
@@ -56,7 +52,6 @@ loader.load(
     console.log(error);
   }
 );
-
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
@@ -71,8 +66,6 @@ document.body.appendChild(stats.dom)
 
 var animate = function () {
   requestAnimationFrame(animate)
-
-  controls.update()
 
   render()
 
