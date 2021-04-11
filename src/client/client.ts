ada7243d04df1e0cb0ed47c042ea184021b25f62
IvanFarkas <Ivan.Farkas@3DHDSoft.com>
2021-04-11 18:51:15 -0400

    Update

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,6 +1,6 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import Stats from 'three/examples/jsm/libs/stats.module'
 import { GUI } from 'three/examples/jsm/libs/dat.gui.module'
 
@@ -8,11 +8,15 @@ const scene: THREE.Scene = new THREE.Scene()
 const axesHelper = new THREE.AxesHelper(5)
 scene.add(axesHelper)
 
-var light = new THREE.PointLight();
-light.position.set(2.5, 7.5, 15)
-scene.add(light);
+var light1 = new THREE.PointLight();
+light1.position.set(2.5, 2.5, 2.5)
+scene.add(light1);
 
-const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+var light2 = new THREE.PointLight();
+light2.position.set(-2.5, 2.5, 2.5)
+scene.add(light2);
+
+const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.01, 1000)
 camera.position.set(0.8, 1.4, 1.0)
 
 const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer()
@@ -23,50 +27,46 @@ const controls = new OrbitControls(camera, renderer.domElement)
 controls.screenSpacePanning = true
 controls.target.set(0, 1, 0)
 
-// AnimationMixer - https://threejs.org/docs/#api/en/animation/AnimationMixer
 let mixer: THREE.AnimationMixer
 let modelReady = false;
 let animationActions: THREE.AnimationAction[] = new Array()
 let activeAction: THREE.AnimationAction
 let lastAction: THREE.AnimationAction
-const fbxLoader: FBXLoader = new FBXLoader();
+const gltfLoader: GLTFLoader = new GLTFLoader();
 
-// Character - Vanguard By T. Choonyung - https://www.mixamo.com/#/?query=vanguard&type=Character
-fbxLoader.load('models/vanguard_t_choonyung.fbx', (object) => {
-  object.scale.set(.01, .01, .01)
-  mixer = new THREE.AnimationMixer(object);
+// Character - Vanguard By T. Choonyung - https://www.mixamo.com/#/?query=vanguard&type=Character - Convert to glb in Blander
+gltfLoader.load('models/vanguard.glb', (gltf) => {
+  // gltf.scene.scale.set(.01, .01, .01)
+  mixer = new THREE.AnimationMixer(gltf.scene);
 
-  let animationAction = mixer.clipAction((object as any).animations[0]);
+  let animationAction = mixer.clipAction((gltf as any).animations[0]);
   animationActions.push(animationAction)
   animationsFolder.add(animations, "default")
   activeAction = animationActions[0]
 
-  scene.add(object);
+  scene.add(gltf.scene);
 
-  // Animation- Samba - https://www.mixamo.com/#/?query=Samba&type=Motion%2CMotionPack
-  fbxLoader.load('models/vanguard@samba.fbx', (object) => {
+  // Animation- Samba - https://www.mixamo.com/#/?query=Samba&type=Motion%2CMotionPack - Convert to glb in Blander
+  gltfLoader.load('models/vanguard@samba.glb', (gltf) => {
     console.log("loaded samba")
-
-    let animationAction = mixer.clipAction((object as any).animations[0]);
+    let animationAction = mixer.clipAction((gltf as any).animations[0]);
     animationActions.push(animationAction)
     animationsFolder.add(animations, "samba")
 
-    // Animation - Belly Dancing - https://www.mixamo.com/#/?query=Belly+Dancing&type=Motion%2CMotionPack
-    fbxLoader.load('models/vanguard@bellydance.fbx', (object) => {
+    // Animation - Belly Dancing - https://www.mixamo.com/#/?query=Belly+Dancing&type=Motion%2CMotionPack - Convert to glb in Blander
+    gltfLoader.load('models/vanguard@bellydance.glb', (gltf) => {
       console.log("loaded bellydance")
-      let animationAction = mixer.clipAction((object as any).animations[0]);
+      let animationAction = mixer.clipAction((gltf as any).animations[0]);
       animationActions.push(animationAction)
       animationsFolder.add(animations, "bellydance")
 
-      // Animation - Goofy Running - https://www.mixamo.com/#/?query=Goofy+Running&type=Motion%2CMotionPack
-      fbxLoader.load('models/vanguard@goofyrunning.fbx', (object) => {
+      // Animation - Goofy Running - https://www.mixamo.com/#/?query=Goofy+Running&type=Motion%2CMotionPack - Convert to glb in Blander
+      gltfLoader.load('models/vanguard@goofyrunning.glb', (gltf) => {
         console.log("loaded goofyrunning");
-        (object as any).animations[0].tracks.shift()
-        //console.dir((object as any).animations[0])
-        let animationAction = mixer.clipAction((object as any).animations[0]);
+        (gltf as any).animations[0].tracks.shift() //delete the specific track that moves the object forward while running
+        let animationAction = mixer.clipAction((gltf as any).animations[0]);
         animationActions.push(animationAction)
         animationsFolder.add(animations, "goofyrunning")
-        //console.dir(animationAction)
 
         modelReady = true
       }, (xhr) => {
@@ -74,13 +74,13 @@ fbxLoader.load('models/vanguard_t_choonyung.fbx', (object) => {
       },
         (error) => {
           console.log(error);
-        }
-      )
+        })
     }, (xhr) => {
       console.log((xhr.loaded / xhr.total * 100) + '% loaded')
-    }, (error) => {
-      console.log(error);
-    })
+    },
+      (error) => {
+        console.log(error);
+      })
   }, (xhr) => {
     console.log((xhr.loaded / xhr.total * 100) + '% loaded')
   },
