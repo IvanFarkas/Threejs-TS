2c47f10a11b1c78aba951119b376d8398038f68a
IvanFarkas <Ivan.Farkas@3DHDSoft.com>
2021-04-12 05:35:29 -0400

    Updated

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,93 +1,99 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import Stats from 'three/examples/jsm/libs/stats.module'
 
 const scene: THREE.Scene = new THREE.Scene()
+const axesHelper = new THREE.AxesHelper(5)
+scene.add(axesHelper)
+
+var light = new THREE.SpotLight()
+light.position.set(12.5, 12.5, 12.5)
+light.castShadow = true
+light.shadow.mapSize.width = 1024;
+light.shadow.mapSize.height = 1024;
+scene.add(light)
 
 const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-camera.position.z = 2
+camera.position.x = 15
+camera.position.y = 15
+camera.position.z = 15
 
 const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer()
+renderer.shadowMap.enabled = true
+renderer.outputEncoding = THREE.sRGBEncoding
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
-const raycaster = new THREE.Raycaster();
-const sceneMeshes = new Array()
-let dir = new THREE.Vector3();
-let intersects = new Array()
-
 const controls = new OrbitControls(camera, renderer.domElement)
-controls.addEventListener('change', () => {
-  xLine.position.copy(controls.target)
-  yLine.position.copy(controls.target)
-  zLine.position.copy(controls.target)
 
-  dir.subVectors(camera.position, controls.target).normalize();
+const pickableObjects: THREE.Mesh[] = new Array()
+let intersectedObject: THREE.Object3D | null
+let originalMaterials: { [id: string]: THREE.Material | THREE.Material[] } = {}
+const highlightedMaterial = new THREE.MeshBasicMaterial({ wireframe: true, color: 0x00ff00 })
 
-  raycaster.set(controls.target, dir.subVectors(camera.position, controls.target).normalize())
-  intersects = raycaster.intersectObjects(sceneMeshes, false);
-  if (intersects.length > 0) {
-    if (intersects[0].distance < controls.target.distanceTo(camera.position)) {
-      camera.position.copy(intersects[0].point)
+const loader = new GLTFLoader()
+
+// TODO: Get model
+loader.load('models/simplescene.glb', (gltf) => {
+  gltf.scene.traverse(function (child) {
+    if ((<THREE.Mesh>child).isMesh) {
+      let m = <THREE.Mesh>child
+
+      // The sphere and plane will not be mouse picked. THe plane will receive shadows while everything else casts shadows.
+      switch (m.name) {
+        case "Plane":
+          m.receiveShadow = true
+          break
+        case "Sphere":
+          m.castShadow = true
+          break
+        default:
+          m.castShadow = true
+          pickableObjects.push(m)
+
+          // Store reference to original materials for later
+          originalMaterials[m.name] = (m as THREE.Mesh).material;
+      }
     }
-  }
-})
-
-const floor = new THREE.Mesh(new THREE.PlaneBufferGeometry(10, 10), new THREE.MeshNormalMaterial({ side: THREE.DoubleSide }))
-floor.rotateX(-Math.PI / 2)
-floor.position.y = -1
-scene.add(floor)
-sceneMeshes.push(floor)
-
-const wall1 = new THREE.Mesh(new THREE.PlaneBufferGeometry(2, 2), new THREE.MeshNormalMaterial({ side: THREE.DoubleSide }))
-wall1.position.x = 4
-wall1.rotateY(-Math.PI / 2)
-scene.add(wall1)
-sceneMeshes.push(wall1)
-
-const wall2 = new THREE.Mesh(new THREE.PlaneBufferGeometry(2, 2), new THREE.MeshNormalMaterial({ side: THREE.DoubleSide }))
-wall2.position.z = -3
-scene.add(wall2)
-sceneMeshes.push(wall2)
-
-const cube: THREE.Mesh = new THREE.Mesh(new THREE.BoxBufferGeometry(), new THREE.MeshNormalMaterial())
-cube.position.set(-3, 0, 0)
-scene.add(cube)
-sceneMeshes.push(cube)
-
-const ceiling = new THREE.Mesh(new THREE.PlaneBufferGeometry(10, 10), new THREE.MeshNormalMaterial({ side: THREE.DoubleSide }))
-ceiling.rotateX(Math.PI / 2)
-ceiling.position.y = 3
-scene.add(ceiling)
-sceneMeshes.push(ceiling)
-
-// Crosshair
-const lineMaterial = new THREE.LineBasicMaterial({
-  color: 0x0000ff
+  })
+  scene.add(gltf.scene)
+}, (xhr) => {
+  console.log((xhr.loaded / xhr.total * 100) + '% loaded')
+}, (error) => {
+  console.log(error);
 });
-var points = new Array();
-points[0] = new THREE.Vector3(-.1, 0, 0);
-points[1] = new THREE.Vector3(.1, 0, 0);
-let lineGeometry = new THREE.BufferGeometry().setFromPoints(points);
-const xLine = new THREE.Line(lineGeometry, lineMaterial);
-scene.add(xLine);
-points[0] = new THREE.Vector3(0, -.1, 0);
-points[1] = new THREE.Vector3(0, .1, 0);
-lineGeometry = new THREE.BufferGeometry().setFromPoints(points);
-const yLine = new THREE.Line(lineGeometry, lineMaterial);
-scene.add(yLine);
-points[0] = new THREE.Vector3(0, 0, -.1);
-points[1] = new THREE.Vector3(0, 0, .1);
-lineGeometry = new THREE.BufferGeometry().setFromPoints(points);
-const zLine = new THREE.Line(lineGeometry, lineMaterial);
-scene.add(zLine);
 
-window.addEventListener('resize', onWindowResize, false)
-function onWindowResize() {
+window.addEventListener('resize', () => {
   camera.aspect = window.innerWidth / window.innerHeight
   camera.updateProjectionMatrix()
   renderer.setSize(window.innerWidth, window.innerHeight)
   render()
+}, false)
+
+const raycaster = new THREE.Raycaster()
+let intersects: THREE.Intersection[]
+
+document.addEventListener('mousemove', onDocumentMouseMove, false);
+function onDocumentMouseMove(event: MouseEvent) {
+  raycaster.setFromCamera({
+    x: (event.clientX / renderer.domElement.clientWidth) * 2 - 1,
+    y: -(event.clientY / renderer.domElement.clientHeight) * 2 + 1
+  }, camera);
+  intersects = raycaster.intersectObjects(pickableObjects, false);
+
+  if (intersects.length > 0) {
+    intersectedObject = intersects[0].object
+  } else {
+    intersectedObject = null
+  }
+  pickableObjects.forEach((o: THREE.Mesh, i) => {
+    if (intersectedObject && intersectedObject.name === o.name) {
+      pickableObjects[i].material = highlightedMaterial
+    } else {
+      pickableObjects[i].material = originalMaterials[o.name]
+    }
+  })
 }
 
 const stats = Stats()
@@ -95,6 +101,8 @@ document.body.appendChild(stats.dom)
 
 var animate = function () {
   requestAnimationFrame(animate)
+
+  controls.update()
 
   render()
 
