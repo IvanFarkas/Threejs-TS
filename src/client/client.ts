585e26dd333a9af52238efd624978694fcf09733
IvanFarkas <Ivan.Farkas@3DHDSoft.com>
2021-04-15 18:21:14 -0400

    Updated

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,3 +1,5 @@
+// MeshNormalMaterial - https://sbcode.net/threejs/meshnormalmaterial/
+
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import Stats from 'three/examples/jsm/libs/stats.module'
@@ -46,13 +48,12 @@ scene.add(torusKnot)
 
 camera.position.z = 3
 
-window.addEventListener('resize', onWindowResize, false)
-function onWindowResize() {
+window.addEventListener('resize', () => {
   camera.aspect = window.innerWidth / window.innerHeight
   camera.updateProjectionMatrix()
   renderer.setSize(window.innerWidth, window.innerHeight)
   render()
-}
+}, false)
 
 const stats = Stats()
 document.body.appendChild(stats.dom)
@@ -87,7 +88,7 @@ function updateMaterial() {
   material.needsUpdate = true
 }
 
-var animate = function () {
+var animate = () => {
   requestAnimationFrame(animate)
 
   render()
@@ -98,4 +99,5 @@ var animate = function () {
 function render() {
   renderer.render(scene, camera)
 }
+
 animate();
