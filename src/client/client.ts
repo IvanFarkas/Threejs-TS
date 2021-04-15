931ef1903b7b70bd150119547b94da10917d182b
IvanFarkas <Ivan.Farkas@3DHDSoft.com>
2021-04-15 16:22:46 -0400

    Updated

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,3 +1,5 @@
+// GLTF Model Loader - https://sbcode.net/threejs/loaders-gltf/
+
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
@@ -24,31 +26,27 @@ document.body.appendChild(renderer.domElement)
 const controls = new OrbitControls(camera, renderer.domElement)
 
 const loader = new GLTFLoader()
-loader.load(
-  'models/monkey.glb', (gltf) => {
-    gltf.scene.traverse(function (child) {
-      if ((<THREE.Mesh>child).isMesh) {
-        let m = <THREE.Mesh>child
-        m.receiveShadow = true
-        m.castShadow = true
-      }
-      if ((<THREE.Light>child).isLight) {
-        let l = <THREE.Light>child
-        l.castShadow = true
-        l.shadow.bias = -.003
-        l.shadow.mapSize.width = 2048
-        l.shadow.mapSize.height = 2048
-      }
-    })
-    scene.add(gltf.scene);
-  },
-  (xhr) => {
-    console.log((xhr.loaded / xhr.total * 100) + '% loaded')
-  },
-  (error) => {
-    console.log(error);
-  }
-);
+loader.load('models/monkey.glb', (gltf) => {
+  gltf.scene.traverse(function (child) {
+    if ((<THREE.Mesh>child).isMesh) {
+      let m = <THREE.Mesh>child
+      m.receiveShadow = true
+      m.castShadow = true
+    }
+    if ((<THREE.Light>child).isLight) {
+      let l = <THREE.Light>child
+      l.castShadow = true
+      l.shadow.bias = -.003
+      l.shadow.mapSize.width = 2048
+      l.shadow.mapSize.height = 2048
+    }
+  })
+  scene.add(gltf.scene);
+}, (xhr) => {
+  console.log((xhr.loaded / xhr.total * 100) + '% loaded')
+}, (error) => {
+  console.log(error);
+});
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
