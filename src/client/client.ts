a919ca9c7f209ab296b1484b7553e184c6a6a17d
IvanFarkas <Ivan.Farkas@3DHDSoft.com>
2021-04-15 18:19:24 -0400

    Updated

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,3 +1,5 @@
+// MeshLambertMaterial - https://sbcode.net/threejs/meshlambertmaterial/
+
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import Stats from 'three/examples/jsm/libs/stats.module'
@@ -62,13 +64,12 @@ scene.add(torusKnot)
 
 camera.position.z = 3
 
-window.addEventListener('resize', onWindowResize, false)
-function onWindowResize() {
+window.addEventListener('resize', () => {
   camera.aspect = window.innerWidth / window.innerHeight
   camera.updateProjectionMatrix()
   renderer.setSize(window.innerWidth, window.innerHeight)
   render()
-}
+}, false)
 
 const stats = Stats()
 document.body.appendChild(stats.dom)
@@ -120,7 +121,7 @@ function updateMaterial() {
   material.needsUpdate = true
 }
 
-var animate = function () {
+var animate = () => {
   requestAnimationFrame(animate)
 
   render()
@@ -131,4 +132,5 @@ var animate = function () {
 function render() {
   renderer.render(scene, camera)
 }
+
 animate();
