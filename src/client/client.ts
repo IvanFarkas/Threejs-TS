d0e118d6b13ab6ef33ce586bb1eadfd653e748b4
IvanFarkas <Ivan.Farkas@3DHDSoft.com>
2021-04-15 17:23:12 -0400

    Updated

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,3 +1,5 @@
+// Cascaded Shadow Maps (CSM) - https://sbcode.net/threejs/csm/
+
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import Stats from 'three/examples/jsm/libs/stats.module'
@@ -50,43 +52,34 @@ const material = new THREE.MeshPhongMaterial({ color: 0x567d46 })
 
 const mtlLoader = new MTLLoader();
 trees.forEach((tree) => {
-  mtlLoader.load('models/' + tree + '.mtl',
-    (materials) => {
-      materials.preload();
+  mtlLoader.load('models/' + tree + '.mtl', (materials) => {
+    materials.preload();
 
-      const objLoader: OBJLoader = new OBJLoader();
-      objLoader.setMaterials(materials);
-      objLoader.load(
-        'models/' + tree + '.obj',
-        (object) => {
-          object.traverse(function (child) {
-            if ((<THREE.Mesh>child).isMesh) {
-              child.castShadow = true
-            }
-          })
-          for (let i = 0; i < 20; i++) {
-            const copy = object.clone()
-            copy.scale.multiplyScalar(Math.max(Math.random() / 50, .01))
-            copy.rotateY(Math.random() * Math.PI * 2)
-            copy.position.set((Math.random() * 20) - 10, 0, (Math.random() * 20) - 10)
-            scene.add(copy);
-          }
-        },
-        (xhr) => {
-          console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-        },
-        (error) => {
-          console.log('An error happened');
+    const objLoader: OBJLoader = new OBJLoader();
+    objLoader.setMaterials(materials);
+    objLoader.load('models/' + tree + '.obj', (object) => {
+      object.traverse(function (child) {
+        if ((<THREE.Mesh>child).isMesh) {
+          child.castShadow = true
         }
-      );
-    },
-    (xhr) => {
+      })
+      for (let i = 0; i < 20; i++) {
+        const copy = object.clone()
+        copy.scale.multiplyScalar(Math.max(Math.random() / 50, .01))
+        copy.rotateY(Math.random() * Math.PI * 2)
+        copy.position.set((Math.random() * 20) - 10, 0, (Math.random() * 20) - 10)
+        scene.add(copy);
+      }
+    }, (xhr) => {
       console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-    },
-    (error) => {
+    }, (error) => {
       console.log('An error happened');
-    }
-  )
+    });
+  }, (xhr) => {
+    console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+  }, (error) => {
+    console.log('An error happened');
+  })
 })
 
 const planeGeometry: THREE.PlaneGeometry = new THREE.PlaneGeometry(25, 25)
@@ -95,34 +88,33 @@ planeMesh.rotateX(-Math.PI / 2)
 planeMesh.receiveShadow = true;
 scene.add(planeMesh)
 
-window.addEventListener('resize', onWindowResize, false)
-function onWindowResize() {
+window.addEventListener('resize', () => {
   camera.aspect = window.innerWidth / window.innerHeight
   camera.updateProjectionMatrix()
   renderer.setSize(window.innerWidth, window.innerHeight)
   render()
-}
+}, false)
 
 const gui = new GUI()
 const csmFolder = gui.addFolder("CSM")
 csmFolder.add(csm.lightDirection, "x", -1, 1, 0.01)
 csmFolder.add(csm.lightDirection, "y", -1, 1, 0.01)
 csmFolder.add(csm.lightDirection, "z", -1, 1, 0.01)
-csmFolder.add(csm, "lightNear", 1, 1000, 1).onChange(function (value) {
+csmFolder.add(csm, "lightNear", 1, 1000, 1).onChange((event) => {
   for (var i = 0; i < csm.lights.length; i++) {
-    csm.lights[i].shadow.camera.near = value;
+    csm.lights[i].shadow.camera.near = event;
     csm.lights[i].shadow.camera.updateProjectionMatrix();
   }
 })
-csmFolder.add(csm, "lightFar", 1, 1000, 1).onChange(function (value) {
+csmFolder.add(csm, "lightFar", 1, 1000, 1).onChange((event) => {
   for (var i = 0; i < csm.lights.length; i++) {
-    csm.lights[i].shadow.camera.far = value;
+    csm.lights[i].shadow.camera.far = event;
     csm.lights[i].shadow.camera.updateProjectionMatrix();
   }
 })
-csmFolder.add(csm, "lightIntensity", .1, 2, .1).onChange(function (value) {
+csmFolder.add(csm, "lightIntensity", .1, 2, .1).onChange((event) => {
   for (var i = 0; i < csm.lights.length; i++) {
-    csm.lights[i].intensity = value
+    csm.lights[i].intensity = event
   }
 })
 csmFolder.open()
@@ -130,7 +122,7 @@ csmFolder.open()
 const stats = Stats()
 document.body.appendChild(stats.dom)
 
-var animate = function () {
+var animate = () => {
   requestAnimationFrame(animate)
 
   controls.update()
